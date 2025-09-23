4f8c6e9cbab1169b6394fa50e0c3b7b8829b7b0d
DESKTOP-KBADI8T\Dell <philip>
2025-09-23 22:25:07 +0100

    first commit

---
+++ src/test/java/com/munninlabs/springmongo/SpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munninlabs.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
