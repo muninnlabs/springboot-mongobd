26037a247e9f4ef52d080b3cece5377cbceac19f
DESKTOP-KBADI8T\Dell <philip>
2025-09-25 21:18:57 +0100

    using DTO pattern to return user data

---
+++ src/main/java/com/munninlabs/springmongo/Instantiation.java
@@ -0,0 +1,29 @@
+package com.munninlabs.springmongo;
+
+import com.munninlabs.springmongo.domain.User;
+import com.munninlabs.springmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+
+
+}
--- src/main/java/com/munninlabs/springmongo/resources/UserResource.java
+++ src/main/java/com/munninlabs/springmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.munninlabs.springmongo.resources;
 
 import com.munninlabs.springmongo.domain.User;
+import com.munninlabs.springmongo.dto.UserDTO;
 import com.munninlabs.springmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -21,9 +23,10 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
